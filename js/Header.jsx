e6cdf929ab86aec4711bb5c95daa2f1a0bccb2ec
celcoder <celcoder@gmail.com>
2018-04-01 19:18:39 -0400

    font size finished

--- js/Header.jsx
+++ js/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => (
   <div className="header-container">
     <div className="header-banner-container">
       <div className="header-banner-content">
-        <p id="header-text-first">THE WEDDING CELEBRATION OF</p>
+        <p id="header-text-first">The Wedding Celebration of</p>
         <img src="./js/img/banner_header.png" alt="" />
-        <p id="header-text-last">05.28.18 MEMORIAL DAY 7PM</p>
+        <p id="header-text-last">05.28.18 Memorial Day 7PM</p>
       </div>
     </div>
   </div>
