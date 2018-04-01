4f82cd305fdfd72c032378bef3de331b8c6614a1
celcoder <celcoder@gmail.com>
2018-04-01 18:57:06 -0400

    fixed margin

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,3 +3,5 @@ import { render } from 'react-dom';
 import App from './App';
 
 render(<App />, document.getElementById('app'));
+
+/* google map api key AIzaSyACc2__sVFWPLcHbVpQxWQj2YrquHhA7c8 */ 
--- js/Header.jsx
+++ js/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => (
   <div className="header-container">
     <div className="header-banner-container">
       <div className="header-banner-content">
-        <p>THE WEDDING CELEBRATION OF</p>
+        <p id="header-text-first">THE WEDDING CELEBRATION OF</p>
         <img src="./js/img/banner_header.png" alt="" />
-        <p>05.28.18 MEMORIAL DAY 7PM</p>
+        <p id="header-text-last">05.28.18 MEMORIAL DAY 7PM</p>
       </div>
     </div>
   </div>
