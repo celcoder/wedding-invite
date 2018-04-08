521669de353192b621fd7a11fca698c1618cd66f
celcoder <celcoder@gmail.com>
2018-04-08 00:39:04 -0400

    almost done

--- js/Footer.jsx
+++ js/Footer.jsx
@@ -5,7 +5,9 @@ import './styles/Footer.css';
 const Footer = () => (
   <div className="footer-container">
     <GoogleMap />
-    <div className="footer-text-container" />
+    <div className="footer-text-container">
+      <p id='made-by'>Designed with by Jesse</p>
+    </div>
   </div>
 );
 
