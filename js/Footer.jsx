f12710016413b205b6f8e6bdce733c3ab1814cbc
celcoder <celcoder@gmail.com>
2018-04-09 14:38:33 -0400

    heart emoji added

--- js/Footer.jsx
+++ js/Footer.jsx
@@ -6,7 +6,9 @@ const Footer = () => (
   <div className="footer-container">
     <GoogleMap />
     <div className="footer-text-container">
-      <p id='made-by'>Designed by Jesse and coded by Ethan with </p>
+      <p id="made-by">
+        Designed by <a href="" className="url-link">Jesse</a> and coded by <a href="" className="url-link">Ethan</a> with <span role="img" aria-label="Heart" id="heart">❤️</span>
+      </p>
     </div>
   </div>
 );
